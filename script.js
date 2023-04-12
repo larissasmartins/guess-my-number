2c7e5e3880abbdb99fd31a006a59110fb9c8d676
Larissa Martins <larissa.martins@inboundcycle.com>
2023-04-11 21:14:08 -0300

    Feat: add style to the game according to the player's actions

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let guess = Number(document.querySelector('.guess').value);
     //when there's no input
     if (!guess) {
         document.querySelector('.message').textContent = 'Please type a number!';
-    } else if (guess < 1 || guess > 50) { 
+    } else if (guess < 1 || guess > 50) { //when it's not a valid name
         document.querySelector('.message').textContent = 'Please type a number between 1 and 50!';
     } else if (guess > secretNumber){
         if (score > 1) {
@@ -26,7 +26,8 @@ let guess = Number(document.querySelector('.guess').value);
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.score').textContent = 0;
-
+            document.body.style.backgroundColor = '#FF0000';
+            document.querySelector('.message').textContent = 'Game over!';
         }
     } else if (guess < secretNumber) {
         if (score > 1) {
@@ -35,10 +36,14 @@ let guess = Number(document.querySelector('.guess').value);
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.score').textContent = 0;
-     
+            document.body.style.backgroundColor = '#FF0000';
+            document.querySelector('.message').textContent = 'Game over!';        
         }
         //when player wins the game
     } else if (guess === secretNumber) {
+        document.body.style.backgroundColor = '#32CD32';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').style.padding = '6rem';
         document.querySelector('.message').textContent = 'Correct number!';
     }
 })
